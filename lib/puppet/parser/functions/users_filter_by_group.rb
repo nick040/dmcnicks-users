f5dbe5820100b0cfd525bdaf47e0c353bd998aae
David McNicol <david@mcnicks.org>
2015-05-03 13:35:25 +0100

    Making sure users only appear once in selected hash

--- lib/puppet/parser/functions/users_filter_by_group.rb
+++ lib/puppet/parser/functions/users_filter_by_group.rb
@@ -6,6 +6,7 @@ module Puppet::Parser::Functions
     groups = [ args[1] ].flatten
     groups.each do |group|
       users.each do |name,attrs|
+        next if selected.key?(name)
         next unless attrs.key?('groups')
         selected[name] = attrs if [ attrs['groups'] ].flatten.include?(group)
       end
