08ffadf24af35a188bbefcc537413e58f0ab14f0
David McNicol <david@mcnicks.org>
2015-05-03 13:20:02 +0100

    Added function to filter hash of users based on groups they are in

---
+++ lib/puppet/parser/functions/users_filter_by_group.rb
@@ -0,0 +1,15 @@
+module Puppet::Parser::Functions
+  newfunction(:users_filter_by_group, :type => :rvalue, :arity => 2) do |args|
+    users = args[0]
+    selected = Hash.new
+    return selected unless users.is_a?(Hash)
+    groups = [ args[1] ].flatten
+    groups.each do |group|
+      users.each do |name,attrs|
+        next unless attrs.key?('groups')
+        selected[name] = attrs if [ attrs['groups'] ].flatten.include?(group)
+      end
+    end
+    return selected
+  end
+end
