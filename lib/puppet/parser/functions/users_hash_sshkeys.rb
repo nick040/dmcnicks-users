fcefc6ba487557c00cd4697483cea151df49db34
somegithubuser <somegithubuser>
2015-07-13 09:43:30 -0400

    Preserve SSH key comment if it exists

--- lib/puppet/parser/functions/users_hash_sshkeys.rb
+++ lib/puppet/parser/functions/users_hash_sshkeys.rb
@@ -6,7 +6,11 @@ module Puppet::Parser::Functions
     keys = [ args[1] ].flatten
     keys.each do |key|
       parts = key.split
-      comment = "#{user}_#{count}"
+      if parts.count == 3 then
+        comment = "#{parts[2]}_#{user}_#{count}"
+      else
+        comment = "#{user}_#{count}"
+      end
       sshkeyhash[comment] = Hash.new
       sshkeyhash[comment]['type'] = parts[0]
       sshkeyhash[comment]['key'] = parts[1]
